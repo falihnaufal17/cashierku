719b7b63ae36771212f02ec456574099c2c3793f
falihnaufal17 <falihnaufal1700@gmail.com>
2021-08-11 20:47:09 +0700

    setup project

---
+++ src/pages/History/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const History = (p) => {
+    return(
+        <h1>History</h1>
+    )
+}
+
+export default History
---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const Home = (p) => {
+    return(
+        <h1>Home</h1>
+    )
+}
+
+export default Home
